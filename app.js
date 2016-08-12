e779c653ed75215d0cecab2cf245f4fda6ad6b7e
Adriano Wichmann <estudoalegre@gmail.com>
2016-08-11 22:37:39 -0300

    - terceira

--- app.js
+++ app.js
@@ -4,7 +4,27 @@
 var app = new Vue({
     el:"#app",
     data:{
+        teste:'',
         title:"Contas a pagar",
+        menus:[
+            {id:0, name:"Listar contas"}, {id:1, name:"Criar contas"}
+        ],
+        activedView: 1,
+        bill:{
+            date_due:'',
+            name :'',
+            value: 0,
+            done: 0
+        },
+        names:[
+            'Conta de luz',
+            'Conta de água',
+            'Conta de telefone',
+            'Supermercado',
+            'Cartão de crédito',
+            'Empréstimo',
+            'Gasolina'
+        ],
         bills:[{date_due:'20/08/2016', name: 'Conta de energia', value:127.99,done:1},
        {date_due:'21/08/2016', name: 'Conta de águra', value:40.99,done:0},
        {date_due:'22/08/2016', name: 'Conta de telefone', value:55.99,done:0},
@@ -24,5 +44,20 @@ var app = new Vue({
 
             return !count?'Nenhuma conta a pagar':'Existem '+count+ ' linhas a pagar';
         }
+    },
+    methods:{
+
+        showView: function ($event, $number) {
+           this.activedView = $number;
+        }
+        ,
+        submit:function () {
+            this.bills.push(this.bill);
+            this.activedView =0;
+        }
     }
+});
+
+app.$watch('teste',function (novoValor, velhoValor) {
+    console.log(velhoValor+" : "+novoValor);
 });
